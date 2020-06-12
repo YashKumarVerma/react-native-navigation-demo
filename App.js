6601a3ecd3199d84d7b615cc07f82abe854bb007
Yash Kumar Verma <yk.verma2000@gmail.com>
2020-06-13 01:37:53 +0530

    project init

--- App.js
+++ App.js
@@ -1,19 +1,73 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, View, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
-export default function App() {
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen{`\n`}</Text>
+      <Button
+        title="Go to Details"
+        onPress={() =>
+          navigation.navigate("Details", {
+            username: "yashkumarverma",
+            message: "something awesome here",
+          })
+        }
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function DetailsScreen({ route, navigation }) {
+  // get the params here
+  const { username } = route.params;
+  const { message } = route.params;
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>these are the details</Text>
+      <Text>ID: {username}</Text>
+      <Text>
+        Message: {message} {`\n`}
+      </Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() =>
+          navigation.push("Details", {
+            username: "someoneElse",
+            message: "new one",
+          })
+        }
+      />
+      <Text>{`\n`}</Text>
+      <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
+      <Text>{`\n`}</Text>
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Text>{`\n`}</Text>
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Some Cool Heading" }}
+        />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
